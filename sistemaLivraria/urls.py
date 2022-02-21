ce54028847fcfd499a69b5e499d183c561b64adc
michelclemer <michelclemermonteirodeolino@gmail.com>
2022-02-21 09:13:54 -0300

    feat: implementing to edit and delete

--- sistemaLivraria/urls.py
+++ sistemaLivraria/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('livros', views.livros, name='livros'),
     path('livros/criar', views.criar, name='criar'),
     path('livros/editar', views.editar, name='editar'),
+    path('deletar/<int:id>', views.deletar, name='deletar'),
+    path('livros/editar/<int:id>', views.editar, name='editar'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sistemaLivraria/views.py
+++ sistemaLivraria/views.py
@@ -1,5 +1,5 @@
 from urllib.request import Request
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Livro
 from .forms import LivroForm
@@ -17,10 +17,23 @@ def livros(request):
     livros = Livro.objects.all()
     print(livros)
     return render(request, 'livros/index.html', {'livros': livros})
-    
+
 def criar(request):
-    formulario = LivroForm(request.POST or None)
+    formulario = LivroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('livros')
     return render(request, 'livros/criar.html', {'formulario': formulario})
 
-def editar(request):
-    return render(request, 'livros/editar.html')
+def editar(request,id):
+    livro = Livro.objects.get(id=id)
+    formulario = LivroForm(request.POST or None, request.FILES or None, instance=livro)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('livros')
+    return render(request, 'livros/editar.html', {'formulario': formulario})
+
+def deletar(request, id):
+    livro = Livro.objects.get(id=id)
+    livro.delete()
+    return redirect('livros')
